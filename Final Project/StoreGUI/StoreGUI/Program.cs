95d4ec9743a1d563766c11af93ce3bb94a77ca7b
Frank Piper <80231726+fpiper08@users.noreply.github.com>
2024-05-02 17:14:15 -0400

    Changed form names
    I changed the names of the forms to allow for easier understanding of what each is expected to do.

--- Final Project/StoreGUI/StoreGUI/Program.cs
+++ Final Project/StoreGUI/StoreGUI/Program.cs
@@ -16,7 +16,7 @@ namespace StoreGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new AppStart());
         }
     }
 }
